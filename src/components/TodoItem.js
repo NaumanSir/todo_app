4bdbe552e61cf495b10d3288bf30346dfdc87ac4
Nauman Husnani <naumanhusnani@gmail.com>
2019-02-07 16:35:40 -0600

    added propTypes and started styling using js class

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
   render() {
     return (
-      <div>
+      <div style={ itemStyle }>
         <p>{ this.props.todo.title }</p>
       </div>
     )
   }
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+const itemStyle = {
+  backgroundColor: "#e6f2ff"
 }
 
 export default TodoItem
