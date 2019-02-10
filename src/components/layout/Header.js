e44b07af2b4cf7498329a4f3683ea6c2311d1d81
Nauman Husnani <naumanhusnani@gmail.com>
2019-02-09 18:14:35 -0600

    added routing with an About page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
       <header style={headerStyle}>
           <h1>Todo List</h1>
+          <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
       </header>
     </div>
   )
@@ -15,4 +17,9 @@ const headerStyle = {
     color: '#fff',
     textAlign: 'center',
     padding: '10px'
+}
+
+const linkStyle = {
+  color: 'white',
+  textDecoration: 'none'
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is my first attempt at React. Thank you to Brad Traversy's crash course for making this possible.</p>
+    </React.Fragment>
+  )
+}
